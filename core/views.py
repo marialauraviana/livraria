9d45694c6c924675f49592a5b89cfccfb72ce808
Maria Laura Viana <marialauraviana@hotmail.com>
2022-07-14 15:17:55 -0300

    commit

--- core/views.py
+++ core/views.py
@@ -1,8 +1,24 @@
 from rest_framework.viewsets import ModelViewSet
 
-from core.models import Categoria
+from core.models import Categoria, Editora, Autor, Livro
 from core.serializers import CategoriaSerializer
+from core.serializers import EditoraSerializer
+from core.serializers import AutorSerializer
+from core.serializers import LivroSerializer
 
 class CategoriaViewSet (ModelViewSet):
     queryset = Categoria.objects.all()
     serializer_class = CategoriaSerializer
+
+class EditoraViewSet (ModelViewSet):
+    queryset = Editora.objects.all()
+    serializer_class = EditoraSerializer
+
+class AutorViewSet (ModelViewSet):
+    queryset = Autor.objects.all()
+    serializer_class = AutorSerializer
+
+class LivroViewSet(ModelViewSet):
+    queryset = Livro.objects.all()
+    serializer_class = LivroSerializer
+        
--- livraria/urls.py
+++ livraria/urls.py
@@ -2,10 +2,14 @@ from django.contrib import admin
 from django.urls import include, path
 
 from rest_framework.routers import DefaultRouter
-from core.views import CategoriaViewSet
+from core.views import CategoriaViewSet, EditoraViewSet, AutorViewSet, LivroViewSet
+
 
 router = DefaultRouter()
 router.register(r'categorias', CategoriaViewSet)
+router.register(r'editoras', EditoraViewSet)
+router.register(r'autores', AutorViewSet)
+router.register(r'livros', LivroViewSet)
 
 
 urlpatterns = [
