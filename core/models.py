4bca4ab6031da8598b1fb576596563b6a40c811b
Maria Laura Viana <marialauraviana@hotmail.com>
2022-07-15 08:34:54 -0300

    projeto

--- core/models.py
+++ core/models.py
@@ -25,7 +25,7 @@ class Autor(models.Model):
 
 class Livro(models.Model):
     titulo = models.CharField(max_length=255)
-    ISBN = models.CharField(max_length=32)
+    ISBN = models.CharField(max_length=32, null= True, blank= True)
     quantidade = models.IntegerField()
     preco = models.DecimalField(max_digits=7, decimal_places=2)
     categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT, related_name='livros')
