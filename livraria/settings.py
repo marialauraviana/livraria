334571597d9ae8596779c61debfc08e3d1e8813c
maria <m.arialauravianaa@gmail.com>
2022-08-25 14:13:50 -0300

    Aula 10

--- livraria/settings.py
+++ livraria/settings.py
@@ -118,5 +118,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS = True
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+#STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
