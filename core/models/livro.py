2139d41124de995e3ce5ce53a81e0e2a3adcb175
Maria Laura Viana <marialauraviana@hotmail.com>
2022-09-15 14:09:12 -0300

    pastas - models/serializers/views

---
+++ core/models/livro.py
@@ -0,0 +1,17 @@
+from django.db import models
+from .autor import Autor 
+from .categoria import Categoria 
+from .editora import Editora 
+
+class Livro(models.Model):
+    titulo = models.CharField(max_length=255)
+    ISBN = models.CharField(max_length=32, null= True, blank= True)
+    quantidade = models.IntegerField()
+    preco = models.DecimalField(max_digits=7, decimal_places=2)
+    categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT, related_name='livros')
+    editora = models.ForeignKey(Editora, on_delete=models.PROTECT, related_name='livros')
+
+    def __str__(self): 
+        return f'{self.titulo} ({self.quantidade})'
+
+    autores = models.ManyToManyField(Autor, related_name="livros")
