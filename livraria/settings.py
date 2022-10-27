062a4b35653241fd9685286597822365f04f4df5
Maria Laura <marialauraviana@gmail.com>
2022-10-27 14:26:15 -0300

    Aula 19

--- livraria/settings.py
+++ livraria/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'media',
+    'drf_spectacular',
     'core',
     'corsheaders',
     'rest_framework',
@@ -137,6 +138,7 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
 
 MEDIA_URL = "http://localhost:8000/media/"
@@ -146,3 +148,9 @@ MEDIA_ENDPOINT = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media_files/")
 
 FILE_UPLOAD_PERMISSIONS = 0o640
+
+SPECTACULAR_SETTINGS = {
+    "TITLE": "Livraria API",
+    "DESCRIPTION": "API para gerenciamento de livraria, incluindo endpoints e documentação.",
+    "VERSION": "1.0.0",
+}
--- livraria/urls.py
+++ livraria/urls.py
@@ -10,6 +10,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from media.router import router as media_router
 
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
+
 
 router = DefaultRouter()
 router.register(r'categorias', CategoriaViewSet)
@@ -24,6 +30,18 @@ urlpatterns = [
     path("api/media/", include(media_router.urls)),
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/swagger/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+    path(
+        "api/redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc",
+    ),
+     path("api/", include(router.urls)),
 ]
 
 urlpatterns += static(settings.MEDIA_ENDPOINT,
